30d1aeeaee4804d568e1313c67ef8bccbb7ac4de
Ingvi Hermanns <ingvi.hermannsson@gmail.com>
2016-10-04 21:07:34 +0000

    Added grid and color changing functionality. Grid creation function added

--- grid.js
+++ grid.js
@@ -1,8 +1,28 @@
 
 $(document).ready(function() {
 
-  for(var i = 0; i < 16; i++) {
+  createGrid(25);
 
-    $("#wrapper").append("<div class=grid>Hestur</div>");
+  
+  $(".grid").hover(function(){
+
+    $(this).css("background-color", "red");
+    
+  });
+
+
+  function createGrid(num) {
+
+    for(var i = 0; i <num*num; i++) {
+
+      var nextDiv = $('<div/>', {text:'', 'class': 'grid'});
+      nextDiv.height(800/num).width(800/num);
+      $("#wrapper").append(nextDiv);
+
+    }
   }
+
+
 });
+
+
